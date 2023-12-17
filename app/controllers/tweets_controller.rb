f7f01ef71edf69809176bebbc70cc3c0aa417b29
39980miki <9898miki.1998@gmail.com>
2023-12-17 23:30:32 +0900

    createアクション定義

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,17 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new 
     #Tweetクラスのインスタンス変数を生成 ツイート投稿画面new.html.erbのform_withヘルパーメソッドで使用するため
   end
+
+  def create
+    Tweet.create(tweet_params)
+    #ストロングパラメーターを定義、createメソッドの引数に使用
+    redirect_to '/' #保存後はトップページが表示されるように
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+    #tweesテーブルへ保存できるように
+  end
+
 end
