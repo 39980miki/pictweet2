185bdd64b6112d305e3808a2e0673df4caf486b3
39980miki <9898miki.1998@gmail.com>
2023-12-17 23:12:27 +0900

    Tweetクラスのnewインスタンス生成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,11 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.all #allメソッドで、tweetsテーブル全てのレコードをインスタンス変数に代入
+    @tweets = Tweet.all 
+    #allメソッドで、tweetsテーブル全てのレコードをインスタンス変数に代入
+  end
+
+  def new
+    @tweet = Tweet.new 
+    #Tweetクラスのインスタンス変数を生成 ツイート投稿画面new.html.erbのform_withヘルパーメソッドで使用するため
   end
 end
