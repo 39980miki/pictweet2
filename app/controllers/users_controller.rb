ce48562bd2c4550a04b587aa7bfd7dc203cf89f1
39980miki <9898miki.1998@gmail.com>
2023-12-29 22:46:02 +0900

    解説付け

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @tweets = user.tweets
+    #クリックされたユーザーのidから情報を取得してビューに受け渡す
+    user = User.find(params[:id]) #送られたidをparamsで取得、そのユーザーのレコードを変数userに代入
+    @nickname = user.nickname #ユーザーのnicknameを取り出して@nicknameに代入
+    @tweets = user.tweets #そのユーザーが投稿したツイートを取得して@tweetsに代入
   end
 end
