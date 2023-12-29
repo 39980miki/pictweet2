8e8bf484db77df45a28dc207adbc9d8122a88a34
39980miki <9898miki.1998@gmail.com>
2023-12-29 22:20:30 +0900

    N+1問題解消includesメソッド

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @tweets = Tweet.all 
+    @tweets = Tweet.includes(:user)
     #allメソッドで、tweetsテーブル全てのレコードをインスタンス変数に代入
   end
 
